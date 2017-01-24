183ffe7bd96d46bf1fe9e713b33b239ff3335bb0
Keith <kperkins411@gmail.com>
2017-01-24 14:43:49 -0500

    Added 2 buttons, 2 different handlers (XML and anonymous) and some strings

--- app/src/main/java/com/example/keith/a1_app/MainActivity.java
+++ app/src/main/java/com/example/keith/a1_app/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.keith.a1_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView    mtv;
+    Button      myButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get a handle to UI elements of interest
+        mtv         =(TextView)findViewById(R.id.textView);
+        myButton2   =(Button)findViewById(R.id.button2);
+
+        //manually add an anonymous listener for button2, button1 is handled through XML
+        myButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.mtv.setText(R.string.text_b2);
+            }
+        });
+
+    }
+
+    public void doButton1(View view) {
+        mtv.setText(R.string.text_b1);
     }
 }
